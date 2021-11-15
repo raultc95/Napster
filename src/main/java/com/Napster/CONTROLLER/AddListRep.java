64e03f365446ee3c97d461f5c966307d33953203
raultc95 <raultc95@gmail.com>
2021-11-15 14:28:39 +0100

    El añadir lista de reproduccion muestra las canciones

--- src/main/java/com/Napster/CONTROLLER/AddListRep.java
+++ src/main/java/com/Napster/CONTROLLER/AddListRep.java
@@ -1,8 +1,18 @@
 package com.Napster.CONTROLLER;
 
+import com.Napster.MARIADB.MariaDBSong;
+import com.Napster.MODEL.Song;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class AddListRep {
+    @FXML
+    private TableView<MariaDBSong> listSong;
+    @FXML
+    private TableColumn<Song,String> songCol;
 
 
 
@@ -10,6 +20,19 @@ public class AddListRep {
 
     @FXML
     private void initialize(){
+        tablalista();
+        listSong.setItems(FXCollections.observableList(MariaDBSong.listarTodos()));
+
+    }
+
+    @FXML
+    private void tablalista(){
+        songCol.setCellValueFactory(lista -> {
+            SimpleStringProperty v = new SimpleStringProperty();
+            v.setValue(lista.getValue().getNombre());
+            return v;
+
+        });
 
     }
 }
